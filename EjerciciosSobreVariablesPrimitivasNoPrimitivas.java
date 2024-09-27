b0cf817ea81f550fc7ec834068da3471cf9fc73b
marco <antoni90211@gmail.com>
2024-09-27 12:43:40 -0500

    Subiendo mi tarea desde Eclipse

---
+++ EjerciciosSobreVariablesPrimitivasNoPrimitivas.java
@@ -0,0 +1,35 @@
+package coderHouseJavaClase01;
+
+public class EjerciciosSobreVariablesPrimitivasNoPrimitivas {
+	
+public static voind main (String []args) {
+	
+	/*
+	 * 
+    Crear una clase Persona que tenga variables primitivas como edad y altura.
+    Declarar un arreglo de String para almacenar los nombres de los días de la semana.
+    Definir un ArrayList<Integer> para almacenar las edades de varios estudiantes.
+    Crear un objeto de la clase Persona y asignar valores a sus variables.
+
+
+	 */
+	
+	public class Persona(int edad, double altura){
+		private int edad = 39;
+		private double altura = 1.67;
+		
+	}
+	
+	string [] nombresDiasSemana = {lunes , martes , miercoles, jueves, viernes, sabado, domingo};
+	
+	ArrrayList<interger>edades = new ArrayList<>();
+	
+	edades.add(10);
+	edades.add(9);
+	edades.add(13);
+	
+
+	Persona persona = new Persona(39, "USA", true);
+
+}
+}
